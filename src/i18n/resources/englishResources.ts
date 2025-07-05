af1d2d537e55546f9ace05edd5227e164df131ec
DJOSSA LIONEL <45937654+LionelRostand@users.noreply.github.com>
2025-07-05 06:37:30 +0200

    Update englishResources.ts

--- src/i18n/resources/englishResources.ts
+++ src/i18n/resources/englishResources.ts
@@ -2,6 +2,41 @@ import navigation from '../locales/en/navigation.json';
 import common from '../locales/en/common.json';
 import properties from '../locales/en/properties.json';
 import contractForm from '../locales/en/contractForm.json';
+import contracts from '../locales/en/contracts.json',
+import  dashboard  from '../locales/en/dashboard.json'
+import contracts from '../locales/en/contracts.json',
+import  dashboard  from '../locales/en/dashboard.json';
+import forecasting from '../locales/en/forecasting.json';
+import  help from '../locatles/en/help.json';
+import inspections  from '../locales/en/inspections.json';
+import legal  from '../locales/en/legal.json';
+import  maintenanceCommon from '../locales/en/maintenanceCommon.json';
+import  maintenanceCosts from '../locales/en/maintenanceCosts.json';
+import maintenanceHistory from '../locales/en/maintenanceHistory.json';
+import maintenanceInterventions from '../locales/en/maintenanceInterventions.json';
+import maintenanceResponsibilities from '../locales/en/maintenanceResponsibilities.json';
+import messages from '../locales/en/messages.json';
+import ownerSpace '../locales/en/ownerSpace.json';
+import pages from '../locales/en/pages.json';
+import profile from '../locales/en/profile.json';
+import publicSite from '../locales/en/publicSite.json';
+import quickActions  from '../locales/en/quickActions.json';
+import rentManagement from '../locales/en/rentManagement.json';
+import rentalCharges from '../locales/en/rentalCharges.json';
+import roommateSpace from '../locales/en/roommateSpace.json';
+import roommates from '../locales/en/roommates.json';
+import settings from '../locales/en/settings.json';
+import taxes from '../locales/en/taxes.json';
+import tenantDocuments from '../locales/en/tenantDocuments.json';
+import tenantHistory from '../locales/en/tenantHistory.json';
+import tenantOverview from '../locales/en/tenantOverview.json';
+import tenantPayments from '../locales/en/tenantPayments.json';
+import tenantProfile from '../locales/en/tenantProfile.json';
+import tenantSpace  from '../locales/en/tenantSpace.json';
+import tenantUpload from '../locales/en/tenantUpload.json';
+import tenants from '../locales/en/tenants.json';
+import website from '../locales/en/website.json';
+
 
 export const englishResources = {
   translation: {
@@ -9,6 +44,38 @@ export const englishResources = {
     ...common,
     ...properties,
     ...contractForm,
+    ...contracts,
+...dashboard, 
+...forecasting, 
+...help, 
+...inspections,  
+...legal,  
+...maintenanceCommon, 
+...maintenanceCosts,
+...maintenanceHistory, 
+...maintenanceInterventions, 
+...maintenanceResponsibilities, 
+...messages,
+...ownerSpace, 
+...pages,
+...profile, 
+...publicSite, 
+...quickActions,  
+...rentManagement, 
+...rentalCharges,
+...roommateSpace,
+...roommates,
+...settings,
+...taxes,
+...tenantDocuments, 
+...tenantHistory,
+...tenantOverview, 
+...tenantPayments, 
+...tenantProfile,
+...tenantSpace,
+...tenantUpload, 
+...tenants',
+...website',
     // General translations
     common: {
       loading: "Loading...",
